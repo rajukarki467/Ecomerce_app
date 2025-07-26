0aaf71277151f37f8d817ddaa1fe584d952aff64
rajukarki467 <rajukarki467@gmail.com>
2025-07-26 23:54:44 +0545

    making Login page working

--- electric_shop/lib/pages/login.dart
+++ electric_shop/lib/pages/login.dart
@@ -1,126 +1,212 @@
+import 'package:electric_shop/pages/bottomnav.dart';
 import 'package:electric_shop/pages/signup.dart';
 import 'package:electric_shop/widget/support_widget.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
-class Login extends StatelessWidget {
+class Login extends StatefulWidget {
+  @override
+  State<Login> createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  String email = "", password = "";
+  final _formkey = GlobalKey<FormState>();
+
+  var emailcontroller = TextEditingController();
+  var passwordcontroller = TextEditingController();
+
+  userLogin() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => Bottomnav()),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              'No User Found for that Email',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        );
+      } else if (e.code == 'wrong-password') {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(
+            backgroundColor: Colors.redAccent,
+            content: Text(
+              'Wrong Password Provided by User',
+              style: TextStyle(
+                fontSize: 18.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ),
+        );
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Container(
           margin: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image.asset(
-                'assets/images/login.png',
-                width: MediaQuery.of(context).size.width,
-                height: 220.0,
-                fit: BoxFit.cover,
-              ),
-              Center(
-                child: Text("Sign in", style: AppWidget.boldTextFieldStyle()),
-              ),
-              SizedBox(height: 16.0),
-              Text(
-                "Please enter the details below to \n                    continue.",
-                style: AppWidget.lightTextFieldStyle(),
-              ),
-              SizedBox(height: 28.0),
-              Text("Email", style: AppWidget.semiboldTextFieldStyle()),
-              SizedBox(height: 10.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xfff4f5f9),
-                  borderRadius: BorderRadius.circular(12.0),
+          child: Form(
+            key: _formkey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Image.asset(
+                  'assets/images/login.png',
+                  width: MediaQuery.of(context).size.width,
+                  height: 220.0,
+                  fit: BoxFit.cover,
                 ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Please Enter Your Email",
-                    prefixIcon: Icon(Icons.email),
-                  ),
+                Center(
+                  child: Text("Sign in", style: AppWidget.boldTextFieldStyle()),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Text("Password", style: AppWidget.semiboldTextFieldStyle()),
-              SizedBox(height: 10.0),
-              Container(
-                decoration: BoxDecoration(
-                  color: Color(0xfff4f5f9),
-                  borderRadius: BorderRadius.circular(12.0),
+                SizedBox(height: 16.0),
+                Text(
+                  "Please enter the details below to \n                    continue.",
+                  style: AppWidget.lightTextFieldStyle(),
                 ),
-                child: TextField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    hintText: "Please Enter Your Password",
-                    prefixIcon: Icon(Icons.password),
-                  ),
-                ),
-              ),
-              SizedBox(height: 16.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text(
-                    "Forgot Password?",
-                    style: TextStyle(
-                      color: Colors.green,
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w500,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(height: 16.0),
-              Center(
-                child: Container(
-                  width: MediaQuery.of(context).size.width / 2,
-                  padding: EdgeInsets.all(14.0),
+                SizedBox(height: 28.0),
+                Text("Email", style: AppWidget.semiboldTextFieldStyle()),
+                SizedBox(height: 10.0),
+                Container(
+                  padding: EdgeInsets.only(left: 20.0),
                   decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(20.0),
+                    color: Color(0xfff4f5f9),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
-                  child: Center(
-                    child: Text(
-                      "Login",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 16.0,
-                      ),
+                  child: TextFormField(
+                    controller: emailcontroller,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please Enter your Email";
+                      }
+                      return null;
+                    },
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Please Enter Your Email",
+                      prefixIcon: Icon(Icons.email),
                     ),
                   ),
                 ),
-              ),
-              SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "Don't Have an account?  ",
-                    style: AppWidget.lightTextFieldStyle(),
+                SizedBox(height: 20.0),
+                Text("Password", style: AppWidget.semiboldTextFieldStyle()),
+                SizedBox(height: 10.0),
+                Container(
+                  padding: EdgeInsets.only(left: 20.0),
+                  decoration: BoxDecoration(
+                    color: Color(0xfff4f5f9),
+                    borderRadius: BorderRadius.circular(12.0),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(builder: (context) => Signup()),
-                      );
+                  child: TextFormField(
+                    controller: passwordcontroller,
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please Enter your Password";
+                      }
+                      return null;
                     },
-                    child: Text(
-                      "Sing Up ",
+                    decoration: InputDecoration(
+                      border: InputBorder.none,
+                      hintText: "Please Enter Your Password",
+                      prefixIcon: Icon(Icons.password),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      "Forgot Password?",
                       style: TextStyle(
                         color: Colors.green,
-                        fontSize: 20.0,
-                        fontWeight: FontWeight.bold,
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                GestureDetector(
+                  onTap: () async {
+                    if (_formkey.currentState!.validate()) {
+                      setState(() {
+                        email = emailcontroller.text;
+                        password = passwordcontroller.text;
+                      });
+                    }
+                    userLogin();
+                  },
+                  child: Center(
+                    child: Container(
+                      width: MediaQuery.of(context).size.width / 2,
+                      padding: EdgeInsets.all(14.0),
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        ),
                       ),
                     ),
                   ),
-                ],
-              ),
-            ],
+                ),
+                SizedBox(height: 20.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "Don't Have an account?  ",
+                      style: AppWidget.lightTextFieldStyle(),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context) => Signup()),
+                        );
+                      },
+                      child: Text(
+                        "Sing Up ",
+                        style: TextStyle(
+                          color: Colors.green,
+                          fontSize: 20.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
